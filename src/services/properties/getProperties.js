f34d163db22edd96197b73b45d77640ad598520a
Leiner Acebedo Morales <leiner_acebedo92@yahoo.com>
2024-09-02 22:32:10 +0200

    Comment refering amenity query

--- src/services/properties/getProperties.js
+++ src/services/properties/getProperties.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const getProperties = async (location, pricePerNight) => {
     const prisma = new PrismaClient();
-
+    // The query parameter for this endpoint referin the amenities was not implemented, because there is no connection between properties and amenities in the data. I ask to one of the mentors in Slack and he say it wasnt imortant. Otherwise let me know.
     return prisma.property.findMany({
         where: {
             location,
